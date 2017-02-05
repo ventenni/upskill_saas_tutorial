03a44e388e87945fa0bd620b039c1fa99f7eab3a
Nick Vaiente <ventenni@gmail.com>
2017-02-05 05:26:38 +0000

    nothing new

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  defailt to: 'nicksemailtests@gmail.com'
+  default to: 'nicksemailtests@gmail.com'
   
   def contact_email(name, email, body)
     @name = name
